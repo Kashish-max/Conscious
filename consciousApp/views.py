0657db25e9b03ef53c891cddcceaff00aadded9f
Chahat Kalsi <chahat.ck88@gmail.com>
2021-01-17 15:53:08 +0530

    fixed an issue with tw detection

--- consciousApp/views.py
+++ consciousApp/views.py
@@ -21,7 +21,7 @@ def braille(request):
 
 def triggers(request):
         if request.method=='POST':
-            text = request.POST['some_text']
+            text = request.POST['some_text'].lower()
             triggers = ["9 11", "9-11", "9/11", "ableism", "abusive", "ageism", "alcoholism", "animal abuse", "animal death", "animal violence", "bestiality", "gore", "corpse", "bully", "cannibal", "car accident", "child abuse", "childbirth", "classism", "death", "decapitation", "abuse", "drug", "heroin", "cocaine", "eating disorder", "anorexia", "binge eating", "bulimia", "fatphobia", "forced captivity", "holocaust", "hitler", "homophobia", "hostage", "incest", "kidnap", "murder", "nazi", "overdose", "pedophilia", "prostitution", "PTSD", "racism", "racist", "rape", "raping", "scarification", "self-harm", "self harm", "cutting", "sexism", "slavery", "slurs", "suicide", "suicidal", "swearing", "terminal illness", "terrorism", "torture", "transphobia", "violence", "warfare"]
             tw = []
             for trigger in triggers:
