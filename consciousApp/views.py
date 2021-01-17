09f46823d75ab67723f55e4a620aff8f33d31fda
Chahat Kalsi <chahat.ck88@gmail.com>
2021-01-17 15:30:58 +0530

    added script to check for TWs

--- consciousApp/views.py
+++ consciousApp/views.py
@@ -22,7 +22,13 @@ def braille(request):
 
 def triggers(request):
         if request.method=='POST':
-            return render(request, 'consciousApp/triggers.html', {'text': request.POST['some_text']})
+            text = request.POST['some_text']
+            triggers = ["9 11", "9-11", "9/11", "ableism", "abusive", "ageism", "alcoholism", "animal abuse", "animal death", "animal violence", "bestiality", "gore", "corpse", "bully", "cannibal", "car accident", "child abuse", "childbirth", "classism", "death", "decapitation", "abuse", "drug", "heroin", "cocaine", "eating disorder", "anorexia", "binge eating", "bulimia", "fatphobia", "forced captivity", "holocaust", "hitler", "homophobia", "hostage", "incest", "kidnap", "murder", "nazi", "overdose", "pedophilia", "prostitution", "PTSD", "racism", "racist", "rape", "raping", "scarification", "self-harm", "self harm", "cutting", "sexism", "slavery", "slurs", "suicide", "suicidal", "swearing", "terminal illness", "terrorism", "torture", "transphobia", "violence", "warfare"]
+            tw = []
+            for trigger in triggers:
+                if text.find(trigger) > -1: tw.append(trigger)
+            if tw == []: tw.append('No Triggers Found')
+            return render(request, 'consciousApp/triggers.html', {'text': text, 'triggers': tw})
         else:
             return render(request, 'consciousApp/triggers.html')
 
