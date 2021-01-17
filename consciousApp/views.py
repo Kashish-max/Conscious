89da95af18bd01fca4efb90c0c0497fd3731bc74
Chahat Kalsi <chahat.ck88@gmail.com>
2021-01-17 10:24:55 +0530

    initial commit;

--- consciousApp/views.py
+++ consciousApp/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse, Http404
 
 def index(request):
     return render(request, 'consciousApp/index.html')
+
+def ocr(request):
+    return render(request, 'consciousApp/ocr.html')
+
+def braille(request):
+    return render(request,'consciousApp/braille.html')
+
+def triggers(request):
+        return render(request, 'consciousApp/triggers.html')
+
